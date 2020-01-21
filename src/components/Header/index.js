4a8e5cc2179431e934afdc9b10817fda65086c90
Wallace Saturnino <wallacesat@live.co.uk>
2020-01-20 23:05:56 -0300

    Finished

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container, Top, Logo, Title,
+} from './styles';
+
+import lofo from '~/assets/Nubank_Logo.png';
 
 export default function Header() {
   return (
-    <View />
+    <Container>
+      <Top>
+        <Logo source={lofo} />
+        <Title>Wallace</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+    </Container>
   );
 }
